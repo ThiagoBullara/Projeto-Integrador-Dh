7d813590dfb1155c5076b18d645a97a58b820c7d
Luis Felipe Ol <l.ufelipe@outlook.com>
2020-06-24 16:26:37 -0300

    sobre nos finalizado, sem imagens finais

--- PaginasSobreNos/sobrenos.php
+++ PaginasSobreNos/sobrenos.php
@@ -12,87 +12,78 @@
 
         <?php require_once('../Header,footer,index/header.php')?>
 
-        <section class="image-carousel"> 
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div id="carouselExampleCaptions" class="carousel-slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                            </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="d-block w-100" alt="Equipe">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Cooperação e União</h5>
-                                    <p>Uma empresa que preza pela formação e união, para inovar</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://images.unsplash.com/photo-1461009312844-e80697a81cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=980&q=80" class="d-block w-100" alt="Brownie">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Brownie da Família</h5>
-                                    <p>Tenha a experiência de passar mal com essa receita incrível</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://images.unsplash.com/photo-1573496130141-209d200cebd8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" class="d-block w-100" alt="Dupla">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>Reconhecimento por Qualidade</h5>
-                                    <p>Comprometida a entregar as melhores soluções</p>
-                                </div>
-                            </div>
-                        </div>
-                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section><br><br>
 
-
-        <div class="textosobrenos">
-            <div class="container-fluid">
-            <div class="row">
-                <h1>O que é a BuyHood</h1>
-                <div class="col-lg-"></div>
-                <div class="col-lg-10">
-                    <p>Somos a BuyHood, uma uma empresa jovem e brasileira, comprometida a trazer novas experiências gastrônomicas para nossos clientes.<br>
+        <br><div class= "container-fluid" >
+            <div class = "row" > 
+                <div class= "conteudo1 col-lg-6">
+                   <h1>Sobre nós</h1>
+                   <p>Somos a BuyHood, uma uma empresa jovem e brasileira, comprometida a trazer novas experiências gastrônomicas para nossos clientes.<br>
                     Nascemos com o objetivo de facilitar o cotidiano de quem busca o diferente. Trazendo a gastronomia para mais perto das pessoas.<br>
-                    Viva novos momentos conosco.
-                    </p>
+                    Viva novos momentos conosco.</p>
                 </div>
-                <br><br>
-
-                <h1>bananada dot com</h1>
-                <div class="col-lg-"></div>
-                <div class="col-lg-10">
-                    <p>tttttttttttttttttttttttttttttttttt
-                    </p>
-                </div><br><br>
-
-                <h1>ricao come árvore</h1>
-                <div class="col-lg-"></div>
-                <div class="col-lg-10">
-                    <p>tttttttttttttttttttttttttttttttttt
-                    </p>
+                
+                <div class= "conteudo1 col-lg-6">
+                   <img src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" alt=" " width="300px">
                 </div>
-                <br><br>
+            </div><br>
+            <!-- barra de divisao -->
+            <div class = "row forewers col-lg-12" style= "width:100%; height:45px; background-color: #000; margin: 0;">
+                <div class = "container"></div>
+            </div><br>
+            <!-- barra de divisao -->
+            <div class = "row">
+                <div class= "conteudo1 col-lg-6">
+                   <img src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1190&q=80" alt=" " width="300px">
+                </div>
+                
+                <div class= "conteudo2 col-lg-6">
+                    <h1>O que fazemos?</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis corporis aspernatur laudantium? Nulla quasi veritatis delectus dolor quod? Dolores dolorum ipsam, consequatur soluta necessitatibus eaque quidem animi cumque architecto dolorem?</p>
+                </div>
 
-                <div class="col-lg-"></div>
-            </div>
-            </div>
+            </div><br>
+
+
+            <div class = "row" > 
+                <div class= "conteudo1 col-lg-6">
+                   <h1>O que faz de nós a difereça</h1>
+                   <p>Somos a BuyHood, uma uma empresa jovem e brasileira, comprometida a trazer novas experiências gastrônomicas para nossos clientes.<br>
+                    Nascemos com o objetivo de facilitar o cotidiano de quem busca o diferente. Trazendo a gastronomia para mais perto das pessoas.<br>
+                    Viva novos momentos conosco.</p>
+                </div>
+                
+                <div class= "conteudo1 col-lg-6">
+                   <img src="https://images.unsplash.com/photo-1590931511005-cdbb4becdd27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt=" " width="300px">
+                </div>
+            </div><br>
+            <!-- barra de divisao -->
+            <div class = "row forewers col-lg-12" style= "width:100%; height:45px; background-color: #000;margin: 0">
+                <div class = "container"></div>
+            </div><br>
+            <!-- barra de divisao -->
+
+            <div class = "row tripla">
+                <div class = "imagens col-lg-4">
+                    <img src="https://images.unsplash.com/photo-1592234849655-701ac008939b?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" width="300px">
+                </div>
+
+                <div class = "imagens col-lg-4">
+                    <img src="https://images.unsplash.com/photo-1575292547620-d515a1536d24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt="" width="300px">
+                </div>
+
+                <div class = "imagens col-lg-4">
+                    <img src="https://images.unsplash.com/photo-1588095074690-4347a8bfa5ed?ixlib=rb-1.2.1&auto=format&fit=crop&w=584&q=80" alt="" width="300px">
+                </div>
+            </div><br>
+
+                
         </div>
 
+
+</div>
+
+
+        
         <?php include('../Header,footer,index/footer.php')?>
        
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>

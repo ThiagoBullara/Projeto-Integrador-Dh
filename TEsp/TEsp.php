5dbf3d41da9b6555e4a1334b055c614d20b1d301
Henrique Ewers <henriquuevers@hotmail.com>
2020-06-24 05:21:42 -0300

    versao 2 experiencias

---
+++ TEsp/TEsp.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="https://i.ibb.co/q7Wkrgq/icon.png">
+    <link rel="icon" href="https://o.remove.bg/downloads/f0e8a96a-b51c-4828-8192-c4bd441321b4/icon-removebg-preview.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="TEsp.css">
+
+    <title>Todas as experiências</title>
+</head>
+<body>
+    <section class="container-fluid">
+            <div class="banner row">
+                <div class="col-lg-12">
+                    <div class="banner-info">
+                        <h2>bem-vindo</h2>
+                        <h1>Venha ter experiências incriveis</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, nobis quisquam magnam excepturi labore natus deserunt asperiores provident error incidunt vitae, laborum, molestias aperiam maxime facere perferendis pariatur odit obcaecati.</p>
+                    </div>
+                </div>
+    </section>
+    <section class="container-fluid">
+        <div class="row tarja">
+            <div class="col-lg-12">
+                <h2>Aprenda a cozinhar com grandes chefes</h2>
+            </div>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>

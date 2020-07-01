3c8c529211f65e920ed5de937da7ebd1e910ccb9
Raphael Scandura <raphaelscandura@hotmail.com>
2020-07-01 16:33:50 -0300

    Creating new pages

---
+++ Careers/Careers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="https://o.remove.bg/downloads/f0e8a96a-b51c-4828-8192-c4bd441321b4/icon-removebg-preview.png">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="Careers.css">
+        <title>Buyhood - Carreiras</title>
+    </head>
+    <body>
+        <?php require_once('../Header,footer,index/header.php')?>
+
+    <div class="container">
+        <section>
+            <div class="banner">
+                <div class="banner-title">
+                <h1>Trabalhe na Buyhood!</h1>
+                </div>
+                <div class="banner-description">
+                    <p>Nós somos uma empresa de pequeno porte que atua na área de Moema e Vila Nova Conceição, com o escritório em boa localização, e oferecemos salário competitivo, chance de crescimento na carreira, ambiente amigável e café grátis! Junte-se a uma equipe amorosa e cheia de confusão!</p>
+                </div>
+            </div>
+        </section>
+        <hr>
+        <section>
+            <div class="careers-available">
+                <div class="careers-icon">
+                    <span>:(</span>
+                </div>
+                <div class="careers-description">
+                    <span>Oh não! Infelizmente não temos vagas abertas no momento! Até um outro dia..</span>
+                </div>
+            </div>
+        </section>
+    </div>
+        
+        <?php include('../Header,footer,index/footer.php')?>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>

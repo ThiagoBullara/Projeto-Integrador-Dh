f08f812b12d306c2abb1a492f4585852566ecdf1
Raphael Scandura <raphaelscandura@hotmail.com>
2020-07-01 18:00:56 -0300

    Small fix in footer links

--- Header,footer,index/footer.php
+++ Header,footer,index/footer.php
@@ -52,17 +52,17 @@ footer{
 }
 
 .button-footer {
-            list-style: none;
-            font-size: 17px;
-        }
+    list-style: none;
+    font-size: 17px;
+}
 
-        .button-footer a {
-            text-decoration: none;
-            border: 1px solid white;
-            padding: 10px 20px !important;
-            border-radius: 5px;
-            color: white;
-        }
+    .button-footer a {
+        text-decoration: none;
+        border: 1px solid white;
+        padding: 10px 20px !important;
+        border-radius: 5px;
+        color: white;
+    }
 
         .button-footer a:hover {
             color: #73a942;
@@ -166,10 +166,5 @@ footer{
         </div>
     </footer>
     
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>

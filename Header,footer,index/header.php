c804ebca22fd631c456fd1fd02b19f07a1c26b24
Thiago Leite <thibullara99@gmail.com>
2020-07-01 17:56:23 -0300

    Arrumando alguns erros

--- Header,footer,index/header.php
+++ Header,footer,index/header.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://o.remove.bg/downloads/f0e8a96a-b51c-4828-8192-c4bd441321b4/icon-removebg-preview.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <style>
         * {
             padding: 0; 
@@ -62,8 +55,6 @@
             padding: 0 !important;
         }
     </style>
-</head>
-
     <header>
         <div class="container-fluid">
             <div class="row">
@@ -88,4 +79,3 @@
         </div>
     </header>
 
-</html>

3bb5de8eb834f45ace66f237ebcdce499eb0533e
Raphael Scandura <raphaelscandura@hotmail.com>
2020-07-01 16:36:10 -0300

    Visual update on partner registration, php fixes

--- CadastroParceiro/partnerregistration.php
+++ CadastroParceiro/partnerregistration.php
@@ -76,6 +76,7 @@
 
   <?php require_once('../Header,footer,index/header.php')?>
 
+<section style="margin-bottom: 20px;margin-top: 20px;">
   <div class="container">
     <form method="POST" enctype="multipart/form-data">
       <div class="form-group">
@@ -162,6 +163,7 @@
       </div>
     </form>
   </div>
+</section>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>

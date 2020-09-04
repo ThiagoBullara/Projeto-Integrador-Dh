0833095c4590f10eb956f74ab9549aad3f9eef7c
Thiago Bullara Leite <66804021+ThiagoBullara@users.noreply.github.com>
2020-09-04 19:55:52 -0300

    arrumando um erro

--- ProjetoIntegrador/resources/views/layout.blade.php
+++ ProjetoIntegrador/resources/views/layout.blade.php
@@ -16,10 +16,10 @@
             <div class="row">
                 <div class="logo col-lg-12">
                     <div class="desktop">
-                        <a href="/Home" ><img src="https://i.ibb.co/71JLCrG/Logo-Buy-Hood-removebg-preview.png" alt="Logo Buyhood"></a>
+                        <a href="/home" ><img src="https://i.ibb.co/71JLCrG/Logo-Buy-Hood-removebg-preview.png" alt="Logo Buyhood"></a>
                     </div>
                     <div class="mobile">
-                        <a href="/Home"><img src="https://i.ibb.co/0tNkSBr/Icon.png" alt="Logo Buyhood"></a>
+                        <a href="/home"><img src="https://i.ibb.co/0tNkSBr/Icon.png" alt="Logo Buyhood"></a>
                     </div>
                 </div>
             </div>

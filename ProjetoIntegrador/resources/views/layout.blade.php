d2915c418c7ded081e029a38a0c7b11f6e67cbd1
= <raphaelscandura@hotmail.com>
2020-10-10 11:40:50 -0300

    Barra de pesquisa diferente no index de produtos

--- ProjetoIntegrador/resources/views/layout.blade.php
+++ ProjetoIntegrador/resources/views/layout.blade.php
@@ -22,19 +22,21 @@
                 </div>
                 <ul class="nav-buyhood col-lg-9">    
                     <li class="barra-de-pesquisa">
-                        <form action="/ListaDeExperienciasBuscar" method="GET">
-                            <div class="input-group pesquisa-buyhood pesquisa-buyhood-mobile">
-                                <input type="search" class="form-control" name="tipo" placeholder="Pesquisar na BuyHood">
-                                <div class="input-group-append">
-                                    <button type="submit" class="form-control icone-pesquisar-buyhood">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                                        </svg>
-                                    </button>
+                        @if(!Request::is('ListaDeExperiencias'))
+                            <form action="/ListaDeExperienciasBuscar" method="GET">
+                                <div class="input-group pesquisa-buyhood pesquisa-buyhood-mobile">
+                                    <input type="search" class="form-control" name="tipo" placeholder="Pesquisar na BuyHood">
+                                    <div class="input-group-append">
+                                        <button type="submit" class="form-control icone-pesquisar-buyhood">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                                                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        @endif
                     </li>
                     <li><a class="item-nav-buyhood" href="/home">Inicio</a></li>
                     <li><a class="item-nav-buyhood" href="/ListaDeExperiencias">Experiências</a></li>
